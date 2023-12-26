0f8804ebf09a9676927b53c3e0872a2a6d21c12e
Matheus Fernando Vieira Pinto <vieira.matheusf13@gmail.com>
2023-12-26 14:57:57 -0300

    feat - dynamic card creation

---
+++ src/scripts/engine.js
@@ -0,0 +1,32 @@
+const emojis = [
+    "🤦‍♂️",
+    "🤦‍♂️",
+    "😁",
+    "😁",
+    "🫠",
+    "🫠",
+    "🦉",
+    "🦉",
+    "🐞",
+    "🐞",
+    "🎃",
+    "🎃",
+    "🎁",
+    "🎁",
+    "⚽",
+    "⚽"
+];
+
+let openCards = [];
+
+
+let shuffleEmojis = emojis.sort(() => {
+    Math.random() > 0.5 ? 2: -1
+});
+
+for(let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item"
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
